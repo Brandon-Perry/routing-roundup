ade59493d00ef06d7f2736b10465eddcaf9d020a
brandon-perry <brandonperryofficial@gmail.com>
2020-10-20 15:54:05 -0400

    finished phase 2

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express()
+
+app.set('view engine', 'pug')
+
+
+app.get('/',(req,res) => {
+    res.send('Hello from Express!');
+})
+
+
+app.all('*', (req,res)=> {
+    let rand = Math.floor(Math.random()*100)
+    res.render('template1',{method:req.method, path:req.path, randomNumber:rand})
+})
+
+let port = 8081;
+
+app.listen(port,()=> {
+    console.log(`listening on port ${port}`)
+})
